0719af36653f3ebc2196e7183e7d09a4362b5b0c
HelgaPsycho <105391678+HelgaPsycho@users.noreply.github.com>
2022-06-08 11:06:08 +0300

    LifeStory MVC

--- MyDestini/MyDestini/Controller/ViewController.swift
+++ MyDestini/MyDestini/Controller/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var storyBrain = StoryBrain ()
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    
+    var storyBrain = StoryBrain ()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
+        
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+            storyBrain.NextStoryNumber(choice: sender.currentTitle!)
+            updateUI()
+      
+    }
+    
+    func updateUI() {
+        textLabel.text = storyBrain.getTitleText()
+        button1.setTitle(storyBrain.getChoice1Text(), for: .normal)
+        button2.setTitle(storyBrain.getChoice2Text(), for: .normal)
+        button1.setTitleColor(.white, for: .normal)
+        button2.setTitleColor(.white, for: .normal)
+        
+    }
+    
 }
 
